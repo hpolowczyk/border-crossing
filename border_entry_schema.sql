b46a46a6407174b239a48130780a83a14b498910
hpolowczyk <50588161+hpolowczyk@users.noreply.github.com>
2019-08-21 21:27:54 -0400

    border etl complete

---
+++ border_entry_schema.sql
@@ -0,0 +1,16 @@
+﻿
+-- If table already present
+DROP TABLE "border_entry"
+
+CREATE TABLE "border_entry" (
+    "ID" SERIAL   NOT NULL,
+    "Date" DATE   NOT NULL,
+    "Transportation Type" VARCHAR   NOT NULL,
+    "City" VARCHAR   NOT NULL,
+    "State" VARCHAR   NOT NULL,
+    "Number of Passengers" INT   NOT NULL,
+    CONSTRAINT "pk_border_entry" PRIMARY KEY (
+        "ID"
+     )
+);
+
