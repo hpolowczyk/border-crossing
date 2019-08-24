ac4bfb684910d2567bf0536d576634a6d7d6e254
hpolowczyk <50588161+hpolowczyk@users.noreply.github.com>
2019-08-24 12:23:52 -0400

    Completed DB Script

--- DB Script.sql
+++ DB Script.sql
@@ -8,23 +8,26 @@ CREATE DATABASE border_db
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
+--- CONNECT TO border_db AND THEN RUN THE FOLLOWING
+--- RUN ALL CREATE DDLS BEFORE INSERTING VALUES INTO border_etl
+
 -- Create table to hold FX rates
 CREATE TABLE public.fx_rates
 (
-    ID SERIAL NOT NULL,
+    "ID" SERIAL NOT NULL,
     "Date" VARCHAR,
     "Average_FX" NUMERIC(8, 6),
-    PRIMARY KEY (ID)
+    PRIMARY KEY ("ID")
 );
 
 -- Create table to hold US border entry data
 CREATE TABLE public.border_entry (
     "ID" SERIAL   NOT NULL,
     "Date" VARCHAR   NOT NULL,
-    "Transportation Type" VARCHAR   NOT NULL,
+    "Transportation_Type" VARCHAR   NOT NULL,
     "City" VARCHAR   NOT NULL,
     "State" VARCHAR   NOT NULL,
-    "Number of Passengers" INT   NOT NULL,
+    "Number_of_Passengers" INT   NOT NULL,
     CONSTRAINT "pk_border_entry" PRIMARY KEY (
         "ID"
      )
@@ -44,17 +47,31 @@ CREATE TABLE public.cpi (
 CREATE TABLE public.border_etl (
     "ID" SERIAL NOT NULL,
     "Date" VARCHAR NOT NULL,
+	"FoM" DATE NOT NULL,
     "State" VARCHAR NOT NULL,
     "Total_Passengers" INT NOT NULL,
     "Average_FX" NUMERIC(8, 6) NOT NULL,
-    "CPI" FLOAT NOT NULL
-    CONSTRAINT "border_etl" PRIMARY KEY (
+    "CPI" FLOAT NOT NULL,
+    CONSTRAINT "pk_border_etl" PRIMARY KEY (
         "ID"
      )
-)
+);
 
--- Join tables
-SELECT Date, State, Total_Passengers, Average_FX, CPI
+--- DO NOT RUN THIS UNTIL AFTER PYTHON LOAD HAS BEEN COMPLETED
+
+-- Insert into border_etl
+INSERT INTO border_etl (
+	 "Date","FoM", "State", "Total_Passengers", "Average_FX","CPI"
+)
+SELECT be."Date", to_date(be."Date",'Mon YYYY') AS FoM,be."State", SUM(be."Number_of_Passengers") AS "Total_Passengers", fx."Average_FX", cpi."CPI"
 FROM border_entry AS be
-LEFT JOIN fx_rates AS fx ON be.Date = fx.Date
-LEFT JOIN cpi on be.Date = cpi.Date 
+LEFT JOIN fx_rates AS fx ON be."Date" = fx."Date"
+LEFT JOIN cpi on be."Date" = cpi."Date" 
+GROUP BY be."Date", be."State", fx."Average_FX", cpi."CPI"
+ORDER BY FoM,be."State";
+
+-- Column need for sorting; not necessary since Date column is clearer
+ALTER TABLE border_etl DROP "FoM";
+
+-- Check table
+SELECT * FROM border_etl;
