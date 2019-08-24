603e76ac1cbb0fb8338ec1530b170f8b8f3aa0f7
hpolowczyk <50588161+hpolowczyk@users.noreply.github.com>
2019-08-24 12:43:08 -0400

    Final US Border Schema

--- border_entry_schema.sql
+++ border_entry_schema.sql
@@ -1,14 +1,14 @@
 -- If table already exists
-DROP TABLE border_entry
+DROP TABLE border_entry;
 
 -- Create table to hold US border entry data
 CREATE TABLE public.border_entry (
     "ID" SERIAL   NOT NULL,
-    "Date" DATE   NOT NULL,
-    "Transportation Type" VARCHAR   NOT NULL,
+    "Date" VARCHAR   NOT NULL,
+    "Transportation_Type" VARCHAR   NOT NULL,
     "City" VARCHAR   NOT NULL,
     "State" VARCHAR   NOT NULL,
-    "Number of Passengers" INT   NOT NULL,
+    "Number_of_Passengers" INT   NOT NULL,
     CONSTRAINT "pk_border_entry" PRIMARY KEY (
         "ID"
      )
